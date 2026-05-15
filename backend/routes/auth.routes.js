b1f7b4b503a8d3b01976dbfc96ed1f8bcc7a35ca
Ivan Iraldi, SykoTech <iraldiban@gmail.com>
2026-05-14 21:23:40 -0300

    integracion PT 1

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,9 +5,11 @@ const { register, login } = require("../controllers/user.controller")
 authRoutes.post("/register", async (req, res) => {
     try {
         const user = req.body
+        console.log("Datos recibidos en el backend para registro:", user); // Verificar los datos recibidos
         const result = await register(user)
         res.status(201).json(result)
     } catch (error) {
+        console.error("Error en registro:", error); // Verificar el error
         res.status(500).json({ message: error.message })
     }
 })
@@ -18,6 +20,7 @@ authRoutes.post("/login", async (req, res) => {
         const result = await login(user)
         res.json(result)
     } catch (error) {
+        console.error("Error en login:", error); // Verificar el error
         res.status(401).json({ message: error.message })
     }
 })
